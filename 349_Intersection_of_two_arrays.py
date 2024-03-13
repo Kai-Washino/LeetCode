1895995b5ec9b1fa8d23414c1f82bcb56d541b61
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-03-14 02:30:47 +0900

    Add 349

--- 349_Intersection_of_two_arrays.py
+++ 349_Intersection_of_two_arrays.py
@@ -1,13 +1,27 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        answers = []
-        for num1 in nums1:
-            for num2 in nums2:
-                if  num1 == num2:
-                    same = False
-                    for answer in answers:
-                        if num1 == answer:
-                            same = True                
-                    if not same:
-                        answers.append(num1)
-        return answers
+def quick_sort(data, left, right):
+    i = left  # 左のインデックス
+    j = right  # 右のインデックス
+    pivot = data[(left + right) // 2]  # 軸となる値
+
+    while True:
+        while data[i] < pivot:
+            i += 1
+        while data[j] > pivot:
+            j -= 1
+        if i >= j:
+            break
+        data[i], data[j] = data[j], data[i]  # 交換
+        i += 1
+        j -= 1
+
+    # 再帰処理
+    if left < j:
+        quick_sort(data, left, j)
+    if right > i:
+        quick_sort(data, i, right)
+
+
+if __name__ == "__main__":
+    list = [7,8,3,9,0,0,9,1,5]
+    quick_sort(list, 0, len(list))
+    print(list)
