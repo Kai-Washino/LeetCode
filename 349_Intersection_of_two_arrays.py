a9b3ac1a3df7c8acf2e94101a324a4b206bace02
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-03-14 02:31:00 +0900

    Add 349

--- 349_Intersection_of_two_arrays.py
+++ 349_Intersection_of_two_arrays.py
@@ -1,27 +1,54 @@
-def quick_sort(data, left, right):
-    i = left  # 左のインデックス
-    j = right  # 右のインデックス
-    pivot = data[(left + right) // 2]  # 軸となる値
+class Solution:
+    def quick_sort(self, data: List[int], left: int, right: int):
+        i = left
+        j = right
+        pivot = data[(left + right) // 2]
 
-    while True:
-        while data[i] < pivot:
-            i += 1
-        while data[j] > pivot:
-            j -= 1
-        if i >= j:
-            break
-        data[i], data[j] = data[j], data[i]  # 交換
-        i += 1
-        j -= 1
+        while True:            
+            while data[i] < pivot:
+                i = i + 1
+            while data[j] > pivot:
+                j = j - 1
+            if i >= j:                                 
+                break
 
-    # 再帰処理
-    if left < j:
-        quick_sort(data, left, j)
-    if right > i:
-        quick_sort(data, i, right)
+            data[i], data[j] = data[j], data[i]        
+            i = i + 1
+            j = j - 1
+        
+        if left < i -1:
+            self.quick_sort(data, left, j)
+        if right > j + 1:
+            self.quick_sort(data, i, right)
+        
 
-
-if __name__ == "__main__":
-    list = [7,8,3,9,0,0,9,1,5]
-    quick_sort(list, 0, len(list))
-    print(list)
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        self.quick_sort(nums1, 0, len(nums1) - 1)
+        self.quick_sort(nums2, 0, len(nums2) - 1)
+        print(nums2)
+        nums1 = list(dict.fromkeys(nums1))
+        nums2 = list(dict.fromkeys(nums2))
+        print(nums1)
+        
+        answer = []
+        i = 0
+        j = 0
+        while True:
+            if nums1[i] == nums2[j]:
+                answer.append(nums1[i])
+                if i + 1 < len(nums1) and j + 1 < len(nums2):
+                    i = i + 1
+                    j = j + 1
+                else:
+                    break 
+            elif nums1[i] < nums2[j]:
+                if(i + 1 < len(nums1)):
+                    i = i + 1
+                else:
+                    break
+            elif nums2[j] < nums1[i]:
+                if(j + 1 < len(nums2)):
+                    j = j + 1
+                else:
+                    break
+        return answer
